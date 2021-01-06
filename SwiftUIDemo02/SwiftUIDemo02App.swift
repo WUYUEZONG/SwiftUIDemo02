77f6f4ebb41520405cf2df85b95f56ae55983838
WUYUEZONG <w.yzong@outlook.com>
2021-01-06 13:39:31 +0800

    Initial Commit

---
+++ SwiftUIDemo02/SwiftUIDemo02App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemo02App.swift
+//  SwiftUIDemo02
+//
+//  Created by Ginokeo on 2021/1/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemo02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
